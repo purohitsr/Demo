03941e2f772698548d00dbb12fde111390a3896a
Saurav Ranjan Purohit <sauravp@asiapac.altair.com>
2017-12-03 18:05:26 +0530

    My first git Project

---
+++ src/main/java/com/selenium/test/DemoGitProject/ClassA.java
@@ -0,0 +1,12 @@
+package com.selenium.test.DemoGitProject;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	
+	@Test
+	public void Test1(){
+		System.out.println("In test1");
+	}
+
+}
---
+++ src/test/java/com/selenium/test/DemoGitProject/ClassB.java
@@ -0,0 +1,12 @@
+package com.selenium.test.DemoGitProject;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+	
+	@Test
+	public void Test2(){
+		System.out.println("In Test2");
+	}
+
+}
